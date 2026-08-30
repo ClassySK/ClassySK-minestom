fb17f2172b40468fd8047311f3fe6a9f277f7bed
nova ⭐ <novaa0@outlook.com>
2026-08-30 14:06:51 +0100

    fix instrumentation

--- src/main/java/com/novystxr/classysk/Classysk.java
+++ src/main/java/com/novystxr/classysk/Classysk.java
@@ -1,14 +1,34 @@
 package com.novystxr.classysk;
 
 import ch.njol.skript.Skript;
+import ch.njol.skript.classes.ClassInfo;
+import ch.njol.skript.registrations.Classes;
 import ch.njol.skript.variables.Variables;
+import com.novystxr.classysk.api.classes.*;
 import com.novystxr.classysk.api.fields.SerializableField;
 import com.novystxr.classysk.api.util.Logger;
+import com.novystxr.classysk.api.util.StringUtils;
+import com.novystxr.classysk.api.util.TypedInstanceParser;
 import com.novystxr.classysk.main.MainModule;
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.agent.ByteBuddyAgent;
+import net.bytebuddy.asm.Advice;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.dynamic.ClassFileLocator;
+import net.bytebuddy.dynamic.loading.ClassInjector;
+import net.bytebuddy.dynamic.loading.ClassInjector.UsingInstrumentation.Target;
+import net.bytebuddy.dynamic.loading.ClassReloadingStrategy;
+import net.bytebuddy.matcher.ElementMatchers;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.skriptlang.skript.addon.SkriptAddon;
 import org.skriptlang.skript.registration.SyntaxInfo;
 import org.skriptlang.skript.util.Priority;
+
+import java.io.File;
+import java.lang.instrument.Instrumentation;
+import java.util.Collections;
+import java.util.function.Function;
+import java.util.regex.Pattern;
 
 @SuppressWarnings("UnstableApiUsage")
 public class Classysk extends JavaPlugin {
@@ -24,19 +44,32 @@ public class Classysk extends JavaPlugin {
     @SuppressWarnings("UnstableApiUsage")
     public void onEnable() {
         try {
-            /*ByteBuddyAgent.install();
+            Instrumentation inst = ByteBuddyAgent.install();
+            ClassInjector.UsingInstrumentation
+                .of(new File("."), Target.BOOTSTRAP, inst)
+                .inject(Collections.singletonMap(
+                    new TypeDescription.ForLoadedType(AdviceBridge.class),
+                    ClassFileLocator.ForClassLoader.read(AdviceBridge.class)
+                ));
+
+            Class<?> bridge = Class.forName("com.novystxr.classysk.api.classes.AdviceBridge", true, null);
+            bridge.getDeclaredField("pattern").set(null, Pattern.compile("("+CLASSNAME_PATTERN+") instances?"));
+            bridge.getDeclaredField("processClassInfoResult").set(null, (Function<String, Object>) Classysk::processClassInfoResult);
+
             new ByteBuddy()
                 .redefine(Classes.class)
                 .visit(Advice.to(TypedClassAdvice.class).on(ElementMatchers.named("getClassInfoFromUserInput")))
                 .make()
                 .load(Classes.class.getClassLoader(), ClassReloadingStrategy.fromInstalledAgent());
-            TYPES_ALLOWED = true;*/
-            throw new IllegalStateException("Class specific types currently do not work on minestom due to classloading issues");
+            TYPES_ALLOWED = true;
         } catch (IllegalStateException e) {
             Logger.log("<RED>The ByteBuddy agent failed to install, dynamic agent loading has likely been disabled for this JVM.",
             "The plugin will operate as normal but class-specific types will not be available.",
             "To enable this feature, add <YELLOW>-XX:+EnableDynamicAgentLoading</YELLOW> to your JVM startup flags.<BR>");
             Logger.warning(e.toString());
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
 
         Variables.yggdrasil.registerSingleClass(SerializableField.class, "SerializableField");
@@ -44,5 +77,15 @@ public class Classysk extends JavaPlugin {
 
         addon.localizer().setSourceDirectories("lang", null);
         addon.loadModules(new MainModule());
+    }
+
+    public static Object processClassInfoResult(String matchedPattern) {
+        matchedPattern = StringUtils.getLowerCase(matchedPattern);
+        Class<? extends TypedInstanceWrapper> subclass = ClassManager.getSubclass(matchedPattern);
+
+        return new ClassInfo<>(subclass, "typedinstance")
+            .name("Typed Instance Wrapper")
+            .serializeAs(ClassInstance.class)
+            .parser(new TypedInstanceParser<>());
     }
 }
