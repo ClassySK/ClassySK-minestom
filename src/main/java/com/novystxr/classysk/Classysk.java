5218a27bf3a21b92f7bc3784ed49a55bb98031b3
nova ⭐ <novaa0@outlook.com>
2026-09-17 18:21:32 +0100

    compatibility

--- src/main/java/com/novystxr/classysk/Classysk.java
+++ src/main/java/com/novystxr/classysk/Classysk.java
@@ -47,7 +47,7 @@ public class Classysk extends JavaPlugin {
                     ClassFileLocator.ForClassLoader.read(AdviceBridge.class)));
             Class<?> bridge = Class.forName("com.novystxr.classysk.api.classes.AdviceBridge", true, null);
 
-            bridge.getDeclaredField("pattern").set(null, Pattern.compile("(\\w*) instances?"));
+            bridge.getDeclaredField("pattern").set(null, Pattern.compile("(\\w+) instances?"));
             bridge.getDeclaredField("processClassInfoResult").set(null, (Function<String, Object>) matched -> {
                 matched = StringUtils.getLowerCase(matched);
                 Class<? extends ClassInstance> subclass = ClassManager.getSubclass(matched);
