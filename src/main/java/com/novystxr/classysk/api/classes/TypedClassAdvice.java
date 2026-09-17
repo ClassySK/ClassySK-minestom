0d70d4603cd4d918e50388ef91376ddd5ab6dfff
nova ⭐ <novaa0@outlook.com>
2026-09-17 17:19:41 +0100

    adapt types to minestom

--- src/main/java/com/novystxr/classysk/api/classes/TypedClassAdvice.java
+++ src/main/java/com/novystxr/classysk/api/classes/TypedClassAdvice.java
@@ -4,12 +4,8 @@ import ch.njol.skript.classes.ClassInfo;
 import net.bytebuddy.asm.Advice;
 
 import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class TypedClassAdvice {
-
-    public static final Pattern pattern = Pattern.compile("(\\w*) instances?");
-
     @Advice.OnMethodExit
     static void onExit(@Advice.Argument(0) String input, @Advice.Return(readOnly = false) ClassInfo<?> result) {
         if (result != null) return;
@@ -18,13 +14,5 @@ public class TypedClassAdvice {
         if (matcher.matches()) {
             result = (ClassInfo<?>) AdviceBridge.processClassInfoResult.apply(matcher.group(1));
         }
-    }
-
-    @SuppressWarnings("unchecked")
-    public static <T extends ClassInstance> ClassInfo<T> getClassInfo(Class<T> subclass) {
-        return new ClassInfo<>(subclass, "classinstance")
-            .name("Class Instance")
-            .serializeAs(ClassInstance.class)
-            .parser((Parser<T>) Types.classParser);
     }
 }
